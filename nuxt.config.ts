1326b12ef748e36917756f13a055fba4b5a78302
PuppetMaster <ziyelixiang@gmail.com>
2026-02-19 12:21:46 +0800

    submit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,9 @@ export default defineNuxtConfig({
     head: {
       link: [
         // 核心：Google 搜索抓取的图标
-        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.ico' },
         // Apple 设备图标（建议也加上）
-        { rel: 'apple-touch-icon', href: '/favicon.png' }
+        { rel: 'apple-touch-icon', href: '/favicon.ico' }
       ],
       htmlAttrs: {
         lang: 'zh-CN' // 设置为中文
